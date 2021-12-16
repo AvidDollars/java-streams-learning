143d05d78e6e6dac05e00df4941a2c6570ac6f62
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-16 23:19:10 +0100

    added predicate functional interface

---
+++ src/functional/interface_03/Predicate_01.java
@@ -0,0 +1,36 @@
+package functional.interface_03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Predicate_01 {
+    public static void main(String[] args) {
+
+        // Predicate functional interface has only one abstract method → test(Object)
+        // basically just a filter
+        Predicate<Integer> numberLessThanTen = number -> number < 10;
+        Predicate<Integer> numberIsEven = number -> number % 2 == 0;
+
+        List<Integer> numberList = Arrays.asList(3, 5, 7, 9, 11, 13, 15);
+        getNumbersFilteredBy(numberList, numberLessThanTen); // [3, 5, 7, 9]
+
+        // predicates can be chained with 'and' | 'or' | 'negate' default methods
+        // default method → method is implemented straight inside interface
+        Predicate<Integer> lessThanTenAndOdd = numberLessThanTen.and(numberIsEven);
+        getNumbersFilteredBy(numberList, lessThanTenAndOdd); // []
+    }
+
+    public static List<Integer> getNumbersFilteredBy(List<Integer> numbers, Predicate<Integer> filter) {
+        List<Integer> filteredNumbers = new ArrayList<>();
+
+        for (Integer number: numbers) {
+            if (filter.test(number)) {
+                filteredNumbers.add(number);
+            }
+        }
+
+        return filteredNumbers;
+    }
+}
