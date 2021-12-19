ddc6abc77a44ba83ed887451f1b79141c5137435
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-19 10:05:10 +0100

    added file for 'Function' interface

---
+++ src/functional/interface_03/Function_05.java
@@ -0,0 +1,46 @@
+package functional.interface_03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+
+public class Function_05 {
+    public static void main(String[] args) {
+        // one abstract method in Function interface:
+        // takes argument of type T and returns a value of type R → R apply(T t)
+
+        List<Integer> lengthsOfStrings = new ArrayList<>();
+        List<String> listOfStrings = List.of(
+                "hello", "world", "this", "is",
+                "just", "some", "random", "words"
+        );
+
+        // performance issue: String::length returns int, stringLength returns Integer
+        // int is being boxed into Integer
+        Function<String, Integer> stringLength = String::length;
+
+        // no auto-boxing, int is returned (abstract method = applyAsInt)
+        ToIntFunction<String> stringLengthTwo = String::length;
+
+        for (String str: listOfStrings) {
+            int length = stringLength.apply(str);
+            lengthsOfStrings.add(length);
+        }
+
+        // finding max value in a list
+        List<Integer> numberList = List.of(5, 2, 7, 12, 5, 9, 4, 1, 52, 7, 2, 8);
+        ToIntFunction<List<Integer>> findMax = Function_05::maxValueInList;
+
+    }
+
+    static int maxValueInList(List<Integer> list) {
+        int max = Integer.MIN_VALUE;
+        for (Integer number: list) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
