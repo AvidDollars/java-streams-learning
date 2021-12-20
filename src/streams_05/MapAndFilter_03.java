dd2fd1842f24026bc05d66db885c079eac226b0c
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-20 21:28:42 +0100

    added file with map and filter example

---
+++ src/streams_05/MapAndFilter_03.java
@@ -0,0 +1,41 @@
+package streams_05;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapAndFilter_03 {
+    public static void main(String[] args) {
+        List<Person> personList = List.of(
+                new Person("Michael", 15), new Person("Jack", 12),
+                new Person("Emily", 18), new Person("Anabel", 15),
+                new Person("Adam", 14), new Person("Samuel", 22),
+                new Person("Viktor", 20), new Person("Eva", 15),
+                new Person("Edvard", 19), new Person("Simon", 15)
+        );
+
+        List<String> adults = personList.stream()
+                .filter(person -> person.age >= 18)
+                .map(person -> person.name)
+                .collect(Collectors.toList());
+
+        // adults → [Emily, Samuel, Viktor, Edvard]
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
