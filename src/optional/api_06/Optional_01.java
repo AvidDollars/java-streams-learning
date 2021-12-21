37eca1c79c26a6c259e9ab21fe657a45a257b01d
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-21 21:19:46 +0100

    added introductory file to Optional API

---
+++ src/optional/api_06/Optional_01.java
@@ -0,0 +1,49 @@
+package optional.api_06;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Optional_01 {
+    public static void main(String[] args) {
+        // 'Optional' class → introduced in Java 8
+        // Optional<T> → behaves like container object
+        // when used appropriately indicates the absence of a value
+
+        String personName = "Peter";
+
+        // argument passed to 'of' method can not be null
+        Optional<String> name = Optional.of(personName);
+
+        // getting value
+        name.get();
+        name.isEmpty(); // if value == null
+        name.isPresent(); // if value != null
+
+        Optional<String> mysteryBox = Optional.empty();
+        mysteryBox.orElse("was empty..."); // was empty...
+
+        Optional<String> canBeNull = Optional.ofNullable(null);
+        canBeNull.orElse("was null..."); // was null...
+
+        // e.g. find all 4-digit numbers
+        List<String> numList = List.of("246", "7254", "hello", "2590", "12345", "4444");
+        numList.stream()
+                .forEach(num -> System.out.println(validNumber(num).orElse("invalid")));
+        // forEach → invalid, 7254, invalid, 2590, invalid, 4444
+    }
+
+    static Optional<String> validNumber(String number) {
+        // 4-digit number
+        Pattern pattern = Pattern.compile("^\\d{4}$");
+        Matcher matcher = pattern.matcher(number);
+
+        return matcher.find()
+                // returns string back if there is a match
+                ? Optional.of(matcher.group())
+                // returns empty Optional is there is no match
+                : Optional.empty();
+    }
+}
