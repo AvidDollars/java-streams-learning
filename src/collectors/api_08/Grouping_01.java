ecccc3739462e5aae94f53d4d1ce90d072f4e232
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-23 17:04:14 +0100

    added example with 'groupingBy'

--- src/collectors/api_08/Grouping_01.java
+++ src/collectors/api_08/Grouping_01.java
@@ -3,8 +3,9 @@ package collectors.api_08;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static collectors.api_08.DownstreamCollector_03.getPeople;
 
 public class Grouping_01 {
     public static void main(String[] args) {
@@ -21,5 +22,22 @@ public class Grouping_01 {
                 .collect(Collectors.groupingBy(String::length));
 
         // lengthGroups; → {2=[to], 3=[are], 4=[just, play, ...
+
+
+        // e.g. separation to multiple age fractions
+        List<Person> listOfPersons = getPeople();
+
+        Map<String, List<Person>> ageFractions = listOfPersons.stream()
+                .collect(Collectors.groupingBy(Grouping_01::getAgeGroup));
+
+        // ageFractions; →{middle=[Person{name='Nick', age=22, gender=MALE}, ... }
+    }
+
+    static String getAgeGroup(Person person) {
+        return (person.getAge() < 20)
+                ? "young"
+                : (person.getAge() > 60)
+                    ? "old"
+                    : "middle";
     }
 }
